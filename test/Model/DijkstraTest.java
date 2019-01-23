2538b636bba5cb36816e2f0a4110688e50799e57
Vitor Mariano <vitorcor8@gmail.com>
2019-01-23 01:31:00 -0300

    bash commit

--- test/Model/DijkstraTest.java
+++ test/Model/DijkstraTest.java
@@ -1,8 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** *****************************************************************************
+ * Autor: Vitor Cordeiro e Gustavo Mendes
+ * Componente Curricular: MI - Programação II
+ * Concluido em: 08/02/2018
+ * Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+ * trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+ * apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+ * de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+ * do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+ ***************************************************************************************** */
 package Model;
 
 import java.io.IOException;
@@ -15,42 +20,50 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
+ * Testes de unidade para a classe {@link Dijkstra}.
  *
- * @author gusta
+ * @author Vitor e Gustavo
  */
 public class DijkstraTest {
+
     Cidade nova;
     Cidade nova2;
     Mapa a;
     Leitura b;
     Dijkstra aux;
-        
+
+    /**
+     * Este método é executado antes de cada teste de unidade e serve para criar
+     * cidades auxiliares que serão utilizadas nos testes.
+     *
+     * @throws IOException
+     */
     @Before
     public void setUp() throws IOException {
         nova = new Cidade(0, 0, "Feira de Santana");
         nova2 = new Cidade(100, 200, "Acajutiba");
         b = new Leitura();
         a = b.readFile();
-        aux = new Dijkstra (a.getLista());
+        aux = new Dijkstra(a.getLista());
     }
-    
 
     /**
-     * Test of caminho method, of class Dijkstra.
+     * Este teste de unidade serve para verificar se o caminho criado foi
+     * realmente o menor.
      */
     @Test
     public void testCaminho() {
         ArrayList<Cidade> ref;
-        
+
         ref = aux.caminho(nova, nova2);
-        
+
         assertFalse(ref.isEmpty());
-        
+
         assertEquals(3, ref.size());
-        
+
         assertEquals(nova, ref.get(0));
-        
+
         assertEquals(nova2, ref.get(1));
     }
-    
+
 }
