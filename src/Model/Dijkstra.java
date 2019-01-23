3363217e726116796e25c16d1c9d2c004e66870c
Vitor Mariano <vitorcor8@gmail.com>
2019-01-23 01:36:36 -0300

    bash commit
    Alguns Javadoc

--- src/Model/Dijkstra.java
+++ src/Model/Dijkstra.java
@@ -1,8 +1,8 @@
 /** *****************************************************************************
- * Autor: Vitor Cordeiro e Gustavo Mendes
+ * Autores: Vitor Cordeiro e Gustavo Mendes
  * Componente Curricular: MI - Programação II
  * Concluido em: 08/02/2018
- * Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+ * Declaramos que este código foi elaborado por nós de forma individual e não contém nenhum
  * trecho de código de outro colega ou de outro autor, tais como provindos de livros e
  * apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
  * de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
